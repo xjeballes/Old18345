5657fd799166babf72dc3db94ba5755c59c8cfc0
Glen Escuadra <glen.escuadra@g.msuiit.edu.ph>
2019-04-09 15:08:25 +0800

    added CORS fix

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -14,4 +14,8 @@ def create_app(config_name):
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+
     return app
