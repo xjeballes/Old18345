eaf480a06e217e06db0d46fb805e2c3cae1c916c
Francis <fmscrns@gmail.com>
2019-05-08 16:48:26 +0800

    deleted column profPic_filename_sm in pets as we integrate cloudinary api wherein it does the media transformation

--- app/main/config.py
+++ app/main/config.py
@@ -22,7 +22,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:18345@localhost:5432/BoopIt"
+    SQLALCHEMY_DATABASE_URI = "postgres://mvuhirpwnegrfx:a69aaad86a582680767f5c945c437e5ce3bea8229da158df47633f395c04a2b3@ec2-54-235-167-210.compute-1.amazonaws.com:5432/d55v4lf0t9v2sp"
 
 config_by_name = dict(
     dev=DevelopmentConfig,
